c3b4696395aeaf55508274759d0025c4aec0e09d
Jaishree Kushwah <148569449+Jaishree9898@users.noreply.github.com>
2023-12-15 22:50:08 +0530

    Add files via upload

---
+++ jsfirst.js
@@ -0,0 +1,285 @@
+/* let Firstname = "jaishree"
+ let Lastname = "Kushwah"
+const Mobileno = 7803937746;
+
+console.log(Firstname);
+console.log(Lastname);
+console.log(Mobileno);*/
+
+/*
+let x=10;
+let y=10;
+let z=x+y;
+
+console.log(z); */
+
+/*let Name= "Jaishree"
+let Gender="Female"
+const age=27;
+
+console.log('My name is', Name);
+console.log('My gender is', Gender);
+console.log('My age is', age);*/
+
+/*let a=10;
+let b=10;
+let z=a+b;
+let y=a-b;
+let c=a/b;
+let d=a*b;
+let e=a%b;
+
+console.log('Value of z= ', z);
+console.log('y :>> ', y);
+console.log('c :>> ', c);
+console.log('d :>> ', d);
+console.log('e :>> ', e);*/
+
+/*let x=10;
+let y=10;
+
+console.log(x+y);
+console.log(x-y)
+console.log(x/y);
+console.log(x*y);*/
+
+/*
+let abc=10;
+let xyz=20;
+console.log(abc<xyz);*/
+
+// Decision/Conditional
+
+/*let age=10;
+
+if(age>=18){
+    console.log('You are allowed');
+}
+
+else{
+    console.log('You are not allowed');
+}*/
+
+/*
+let standard=2;
+
+if(standard==1){
+    console.log('Room 1');
+}
+
+else if(standard==2){
+    console.log('Room 2');
+}
+
+else{
+    console.log('Room 3');
+}*/
+
+// Conditions
+// If else
+// Switch case
+// termary OP/ ternary conditions
+// Rating mini
+/*
+switch(user){
+    case 'Admin':
+    console.log('Redirect to the admin page');    
+    break;
+
+    case 'Mentor':
+        console.log('Redirect to mentor page');
+        break;
+
+    default:
+        console.log('I am user');    
+} */
+
+/*let user='student';
+
+switch(user){
+    case 'professor':
+    console.log('Faculty page');   
+    break;
+    
+    case 'student':
+    console.log('Student login page');
+    break;
+
+    default:
+    console.log('Default page ');        
+}*/
+
+/*let user='professor';
+
+switch(user){
+    case 'professor':
+    console.log('Admin Page');    
+    break;
+
+    case 'student':
+    console.log('login');
+    break;
+    
+    default:
+    console.log('any')
+        
+}*/
+/*
+let raining=true;
+
+raining?console.log('yes raining'):console.log('not raining');*/
+
+/* let name=['Jaishree','Shubham','kashish','monika'];
+console.log(name [2]);*/
+
+// Object
+
+/*
+let bankaccount={
+firstname:'jaishree',
+lastname:'Kushwah',
+mobileno:7803937746,
+balance:1000,
+};
+console.log(bankaccount);
+console.log(bankaccount.balance);
+console.log(bankaccount.firstname);
+console.log(bankaccount.lastname);*/
+
+// Date and math
+
+
+/*const now=new Date();
+console.log(now);
+console.log(now .toString());
+console.log(now .getFullYear());
+console.log(now .getMonth());
+console.log(now .getDay());
+console.log(now .getTime());
+console.log(now .getHours());
+console.log(now .getMinutes());
+console.log(now .getSeconds());*/
+
+
+
+
+// Math
+
+// const PI=Math.PI;
+// console.log(PI);
+// console.log(Math.round(PI));
+// console.log(Math.round(9.889));
+// console.log(Math.floor(PI));
+// console.log(Math.ceil(PI));
+// console.log(Math.floor(9.56));
+// console.log(Math.ceil(9.5));
+// console.log(Math.min(10,1,2,3,4,6,15));
+// console.log(Math.max(10,14,15,13,12,11,0));
+
+
+// Random
+// console.log(Math.random()*10);
+// console.log(Math.pow(3,9));
+// console.log(Math.log(2));
+// console.log(Math.sin(90));
+
+// do while, while, for
+
+// do While-
+/*
+let i=0;
+do{
+    console.log("value of i is :",i);
+    i++;
+}
+while(i<=5);*/
+
+
+// white-->>
+/*
+let j=0;
+while(j<=5){
+    console.log("value of j is", j);
+    j++;
+}*/
+
+// For Loop-->>>
+/*for(let i=0; i<=5; i++){
+    console.log('Value is ',i);
+}*/
+
+/*for(let i=0;  i<=10; i++){
+    console.log(' $ {i} * $ {i}= $ {i*i}');
+}*/
+
+/*
+for(let i=0;  i<=10; i++){
+    console.log(i,i,i*i);
+}*/
+
+/*let name=['jaishree','Kushwah','Shubham','yash','shubhi']
+console.log(name);
+console.log(name.length);
+
+console.log(name[3]); 
+console.log(name[name.length-1]);*/
+
+
+// Concatenation
+
+// let arr1=[1,2,3,4];
+// let arr2=[5,6,7,8];
+// let arr3=[9,10,11,12,13,14,15]
+// console.log(arr1.concat(arr2,arr3));
+
+// fill
+// let arr4=[1,2,3,4,5,6,7,8,9];
+//     arr4.fill('Jaishree',2,4)
+//     console.log(arr4);
+
+// let name=['jaishree','kushwah','shubham','kashish'];
+// console.log(name);
+// console.log(name[3]);
+// // console.log(name[name.length-2]);
+// name[5]='Jais';
+// console.log(name);
+
+// let names=new Array('Name1','Name2');
+// // console.log(names);
+// names.push('Jais');
+// names.push('kriti','sona')
+// console.log(names);
+
+// let name=['1','2','0','3','4'];
+// console.log(name.slice(2,3));
+
+
+// Splice
+
+// let name=['1','2','3','4','5'];
+// // name.splice(2,2,'jais','kush');
+// name.splice(2,3,'jais','kush');
+// console.log(name);
+
+// Concatenation
+
+// let arr1=[1,2,3,4,5,6];
+// let arr2=[7,8,9,10];
+// console.log(arr1.concat(arr2));
+
+// Fill
+
+// let arr4=[1,2,3,4,5,6,7,8,9,10];
+// arr4.fill('Jaish',2,5);
+// console.log(arr4);
+
+Includes
+
+let num=[1,2,3,4,5,6,7,8,9];
+console.log(num.includes(5,7));
+
+
+
+
+
+
